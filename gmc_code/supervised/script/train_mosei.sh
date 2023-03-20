6bac86a8e4bfb0fe72c5d253415f89ec43f3f999
dnkhanh45 <yoshokarui@gmail.com>
2023-03-20 11:39:24 +0700

    first commit

---
+++ gmc_code/supervised/script/train_mosei.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=../..
+python main_supervised.py -f with experiment.scenario="mosei" experiment.stage="train_model"
---
+++ gmc_code/unsupervised/ingredients/machine_ingredients.py
@@ -0,0 +1,14 @@
+import os
+import sacred
+
+########################
+# Machine Ingredient  #
+########################
+
+machine_ingredient = sacred.Ingredient("machine")
+
+
+@machine_ingredient.config
+def machine_config():
+    #m_path = "Set_your_path_to_unsupervised_folder_here"
+    m_path = os.path.abspath(os.path.curdir)
